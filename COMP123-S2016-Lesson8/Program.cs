59d14caa32f3389c631a542d9a4ab5d2d59371a2
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-07-08 11:52:48 -0400

    added FileHandling Project

--- COMP123-S2016-Lesson8/Program.cs
+++ COMP123-S2016-Lesson8/Program.cs
@@ -10,6 +10,42 @@ namespace COMP123_S2016_Lesson8
     {
         static void Main(string[] args)
         {
+            int firstNumber;
+            int secondNumber;
+
+            try
+            {
+                Console.Write("Enter an Integer: ");
+                firstNumber = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another Integer: ");
+                secondNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Result: {0:f2}", Divide(firstNumber, secondNumber));
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Error: Please enter an integer");
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("Error: Number too long");
+                Console.WriteLine("Please enter an integer smaller than 2,147,483,647");
+            }
+        }
+
+        private static float Divide(int numerator, int denominator)
+        {
+
+            try
+            {
+                float result = Convert.ToSingle(numerator) / Convert.ToSingle(denominator);
+                return result;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error: " + exception.Message);
+                return 0f;
+            }
         }
     }
 }
---
+++ FileHandling/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileHandling.Models;
+using System.IO;
+
+namespace FileHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            string myfile = "..\\..\\myfile.txt";
+            string directoryName = "..\\..\\Models";
+            string[] listOfFiles;
+
+            if(File.Exists(myfile))
+            {
+                Console.WriteLine("Created: " + File.GetCreationTime(myfile));
+            }
+            else
+            {
+                Console.WriteLine("File does not exist");
+            }
+
+            if(Directory.Exists(directoryName))
+            {
+                listOfFiles = Directory.GetFiles(directoryName);
+                for (int file = 0; file < listOfFiles.Length; file++)
+                {
+                    Console.WriteLine("    " + listOfFiles[file]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Dicrectory does not exist");
+            }
+
+            FileStream outFile = new FileStream("..\\..\\somefile.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(outFile);
+            writer.WriteLine("Hello World!");
+            writer.Close();
+            outFile.Close();
+        }
+    }
+}
